b25c0d45ded03b1b85f65fd879a06b700c9ee807
Adam Vessey <adam@discoverygarden.ca>
2023-03-08 16:01:49 -0400

    Slight updates, and attempt.

--- assets/IslandoraStarterSite.php
+++ assets/IslandoraStarterSite.php
@@ -2,14 +2,32 @@
 
 namespace Islandora;
 
+use Composer\Json\JsonFile
 use Composer\Script\Event;
 use Composer\Util\Platform;
 
+/**
+ * Starter Site Composer helper.
+ */
 class StarterSite {
 
+  /**
+   * Root package installation event callback.
+   *
+   * Expected to be triggered on the `post-root-package-install` event, to track
+   * the version of the project from which a project was derived.
+   *
+   * @see https://getcomposer.org/doc/articles/scripts.md#event-names
+   */
   public static function rootPackageInstall(Event $event) {
-    $pretty_version = $event->getComposer()->getPackage()->getPrettyVersion();
-    var_dump($pretty_version);
+    $composer = $event->getComposer();
+    $package = $composer->getPackage();
+    $version_file = new JsonFile('.starter_site_version');
+    $version_file->write([
+      'pretty-version' => $package->getPrettyVersion(),
+      'version' => $package->getVersion(),
+      'release-date' => $package->getReleaseDate(),
+    ]);
   }
 
 }
